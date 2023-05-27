1f7b8b3b84a53cfdb1c9ab87386545cc07035ff2
Ayush <122601890+Ayushkumar890@users.noreply.github.com>
2023-05-27 17:12:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function ramdomimage(){
+    let img1 = Math.random()*5+1;
+    let d1=Math.floor(img1);
+    let img2 = Math.random()*5+1;
+    let d2=Math.floor(img2);
+    let a="images/dice"+d1+".png"
+    let b="images/dice"+d2+".png"
+    document.querySelector('#dice1').setAttribute('src',a);
+    document.querySelector('#dice2').setAttribute('src',b);
+    if(d1>d2){
+        document.querySelector(".win").innerHTML='player1 win'
+    }
+    else if(d2>d1){
+        document.querySelector(".win").innerHTML='player2 win'
+    }
+    else if(d2==d1){
+        document.querySelector(".win").innerHTML='Draw'
+    }
+    
+}
